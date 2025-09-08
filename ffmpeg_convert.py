52f95188ba9b93d4678a543abf1f11d5cb364f64
foobar <foobar@example.com>
2025-09-09 00:14:58 +0900

    Modify some options.

--- ffmpeg_convert.py
+++ ffmpeg_convert.py
@@ -42,26 +42,34 @@ def get_audio_tracks(input_file):
 def main():
     parser = argparse.ArgumentParser(description='Convert video using frame range and audio mixing.')
     parser.add_argument('input', help='Input video file')
-    parser.add_argument('-sf', type=int, help='Start frame')
-    parser.add_argument('-ef', type=int, help='End frame')
-    parser.add_argument('-at', help='Audio tracks to mix (e.g., 0,2)')
-    parser.add_argument('-o', help='Output video file name')
+    parser.add_argument('-sf', '--start-frame', type=int,
+            help='Starting frame (integer)')
+    parser.add_argument('-ef', '--end-frame', type=int,
+            help='Ending frame (integer)')
+    parser.add_argument('-at', '--audio-tracks', type=str,
+            help='Audio tracks to mix up. If not specify, keep separated tracks. (e.g., \'0,2\')')
+    parser.add_argument('-cv', '--copy-video', action='store_true',
+            help='Copy video track rahter than (re)encode.')
+    parser.add_argument('-nl', '--no-loudnorm', action='store_true',
+            help='Do not use loudnorm filter. loundnorm filter is applied only when --audio-tracks specified.')
+    parser.add_argument('-o', '--output', type=str,
+            help='Output video file name')
     args = parser.parse_args()
 
     # check inputs
     (video_probe, audio_probes) = get_probe(args.input)
-    if args.sf is not None:
-        if args.sf < 0 or int(video_probe['nb_frames']) < args.sf:
-            raise Exception(f'Start frame out of bounds: {args.sf}')
-    if args.ef is not None:
-        if args.ef < 0 or int(video_probe['nb_frames']) < args.ef:
-            raise Exception(f'End frame out of bounds: {args.ef}')
-    if args.sf is not None and args.ef is not None:
-        if args.sf >= args.ef:
-            raise Exception(f'End frame must be larger than Start frame: {args.sf}, {args.ef}')
+    if args.start_frame is not None:
+        if args.start_frame < 0 or int(video_probe['nb_frames']) < args.start_frame:
+            raise Exception(f'Start frame out of bounds: {args.start_frame}')
+    if args.end_frame is not None:
+        if args.end_frame < 0 or int(video_probe['nb_frames']) < args.end_frame:
+            raise Exception(f'End frame out of bounds: {args.end_frame}')
+    if args.start_frame is not None and args.end_frame is not None:
+        if args.start_frame >= args.end_frame:
+            raise Exception(f'End frame must be larger than Start frame: {args.start_frame}, {args.end_frame}')
     i_ats = None
-    if args.at is not None:
-        i_ats = list(map(int, args.at.split(',')))
+    if args.audio_tracks is not None:
+        i_ats = list(map(int, args.audio_tracks.split(',')))
         i_range = range(len(audio_probes))
         for i_at in i_ats:
             if i_at not in i_range:
@@ -70,8 +78,8 @@ def main():
         raise Exception(f'Input file not found: {args.input}')
 
     # output file
-    if args.o:
-        output_file = args.o
+    if args.output:
+        output_file = args.output
     else:
         base, ext = os.path.splitext(os.path.basename(args.input))
         output_file = f"{base}.converted{ext}"
@@ -79,16 +87,15 @@ def main():
     # convert frame to time
     r_frame_rate = fractions.Fraction(video_probe['r_frame_rate'])
     start_time = None
-    if args.sf is not None:
-        start_time = args.sf / r_frame_rate
+    if args.start_frame is not None:
+        start_time = args.start_frame / r_frame_rate
     duration = None
-    if args.ef is not None:
-        end_time = args.ef / r_frame_rate
+    if args.end_frame is not None:
+        end_time = args.end_frame / r_frame_rate
         if start_time is not None:
             duration = end_time - start_time
         else:
             duration = end_time
-    print(duration)
 
     # input stream
     input_kwargs = {}
@@ -98,10 +105,16 @@ def main():
         input_kwargs['t'] = float(duration)
     input_stream = ffmpeg.input(args.input, **input_kwargs)
 
+    # ffmpeg-output option
+    ffkwargs = dict()
+    # video stream
+    if args.copy_video:
+        ffkwargs['vcodec'] ='copy'
+
     # audio stream
     if i_ats is None:
         audio_stream = input_stream.audio
-        c_a = 'copy'
+        ffkwargs['acodec'] = 'copy'
     else:
         _audio_streams = list()
         for i_at in i_ats:
@@ -111,13 +124,17 @@ def main():
             _audio_streams, 'amix',
             inputs=len(_audio_streams),
         )
-        c_a = 'aac'
+        ffkwargs['acodec'] = 'aac'
+        # loundnorm filter
+        if not args.no_loudnorm:
+            audio_stream = ffmpeg.filter(audio_stream, 'loudnorm')
 
     # output
     output = ffmpeg.output(
         input_stream.video,
         audio_stream,
         output_file,
+        **ffkwargs,
     )
     ffmpeg.run(output, overwrite_output=True)
 
